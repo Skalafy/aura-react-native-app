024d442dde642b815130aafc6ec1962b5bded627
IsuruKavi <isurukavindalakshan863@gmail.com>
2024-07-02 17:07:48 +0530

    wrote appwrite logic

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useFonts } from "expo-font";
 
 import { SplashScreen, Stack } from "expo-router";
+import GlobalProvider from "../context/GlobalProvider";
 
 SplashScreen.preventAutoHideAsync()
 
@@ -33,12 +34,15 @@ const RootLayout = () => {
   if (!fontsLoaded && !error) {
     return null;
   }
-  return (<Stack>
+  return (
+    <GlobalProvider>
+  <Stack>
     <Stack.Screen name='index' options={{ headerShown: false }} />
     <Stack.Screen name='(auth)' options={{ headerShown: false }} />
     <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
     {/* <Stack.Screen name='/search/[query]' options={{ headerShown: false }} /> */}
   </Stack>
+  </GlobalProvider>
   )
 }
 
